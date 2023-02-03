7101b4d053bb1011fd5ad83252b511bfc7f6a807
void <void@gmail.com>
2023-02-03 05:17:35 -0800

    Initial Commit

---
+++ double-linked-list/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  double-linked-list
+//
+//  Created by Mark Garcia on 2/3/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
